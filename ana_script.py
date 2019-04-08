d796b97433737dfe8b22a743021754bdfb7b6b6b
Alexander Stasik <35530131+AlexStasik@users.noreply.github.com>
2019-04-08 19:18:35 +0200

    Added new main function

---
+++ ana_script.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import neo
+import utils as ut
+
+list_of_data_sets = ['blue', 'green', 'grey', 'orange', 'purple', 'red']
+
+all_data = dict()
+
+for color in list_of_data_sets:
+    print('Processing '+str(color)+' data set')
+
+    #Read in raw data
+    individual_data_set = dict()
+    data_block = ut.load_dataset(color, path=None)
+
+    #Make rasta data
+    individual_data_set['neo_block'] = data_block
+    individual_data_set['spike_raster'] = ut.rasterize_data(data_block, sf = 1.e3)
+
+    all_data['color'] = individual_data_set
