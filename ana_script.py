0b32cc808c6a7ed02492608c02e89f94146f7419
Angela Taylor <atayl140@uthsc.edu>
2019-04-08 21:34:50 +0200

    I added comments, bitches

--- ana_script.py
+++ ana_script.py
@@ -15,9 +15,9 @@ for color in list_of_data_sets:
     data_block = ut.load_dataset(color, path=None)
 
     #Make rasta data
-    individual_data_set['neo_block'] = data_block
-    individual_data_set['spike_raster'] = ut.rasterize_data(data_block, sf = 1.e3)
-    individual_data_set['spike_trains'] = ut.make_lists_of_spike_trains(data_block)
+    individual_data_set['neo_block'] = data_block # this original data block from neo
+    individual_data_set['spike_raster'] = ut.rasterize_data(data_block, sf = 1.e3)  # spikes in 1s and 0s/
+    individual_data_set['spike_trains'] = ut.make_lists_of_spike_trains(data_block) # spikes as time (in seconds?) 
     individual_data_set['isi'] = ut.make_lists_of_isi(data_block)
 
     all_data[color] = individual_data_set
