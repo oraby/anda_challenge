8c4252663af8cc6a4dbdfc45da889c3ee1ce34f8
crheller <charlieheller95@gmail.com>
2019-04-10 09:55:40 +0200

    remove charlie ISI plot and add Alex's

--- AlexTry.py
+++ AlexTry.py
@@ -1,15 +1,24 @@
-plt.figure(figsize=(10, 10))
-for d in all_data:
-    h = plt.hist(all_data[d]['isi'], histtype='step', density=1, cumulative=False, bins=10000, label=d)
-plt.semilogy()
-plt.xlim(0, 0.05)
-plt.legend(loc='lower right')
+import utils as ut
+import matplotlib.pyplot as plt
+
+# load and calculate the ISIs
+list_of_data_sets = ['blue', 'green', 'grey', 'orange', 'purple', 'red']
+ISIs = {}
+for i, color in enumerate(list_of_data_sets):
+    log.info("loading and plotting ISI for {}".format(color))
+    data_block = ut.load_dataset(color)
+    isi = ut.make_lists_of_isi(data_block)
+    ISIs[color] = isi
 
 
+f, ax = plt.subplots(1, 2)
+for d in list_of_data_sets:
+    ax[0].hist(ISIs[d], histtype='step', density=1, cumulative=False, bins=10000, label=d)
+    ax[1].hist(ISIs[d], histtype='step', density=1, cumulative=True, bins=100000, label=d)
 
-plt.figure(figsize=(10, 10))
-for d in all_data:
-    h = plt.hist(all_data[d]['isi'], histtype='step', density=1, cumulative=True, bins=100000, label=d)
-plt.semilogy()
-plt.xlim(0, 0.05)
-plt.legend(loc='lower right')
+
+for a in ax:
+    a.semilogy()
+    a.set_xlim(0, 0.05)
+
+ax[1].legend(loc='lower right')
