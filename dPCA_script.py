bcf7c62c5bc82309c23f8ba912c7ffe139d66dd3
crheller <charlieheller95@gmail.com>
2019-04-11 22:09:35 +0200

    changing the final plots for dpca

--- dPCA_script.py
+++ dPCA_script.py
@@ -51,43 +51,27 @@ def compute_dPCA(colors=ut.list_of_data_sets, fs=100, win_size=200):
         for ev in ut.trial_events:
             events[ev] = np.argwhere(ev_times[ev][0, :]==1)[0][0] / fs
 
-        for comp in R_transform.keys():
-            # plot first four PCs for each marginalized component
-            f, ax = plt.subplots(1, 1)
-            f.suptitle(comp+", "+color)
-
-            ax.set_title(str(dpca.explained_variance_ratio_[comp][0]*100))
-            ax.plot(time, R_transform[comp][0, :, :].T)
-            ax.legend(un_types)
+        f, ax = plt.subplots(2, 1)
+        for i, comp in enumerate(R_transform.keys()):
+            # plot first PC for each marginalized component
+            ax[i].set_title(comp + ", var explained: " + str(round(dpca.explained_variance_ratio_[comp][0]*100, 2)))
+            ax[i].plot(time, R_transform[comp][0, :, :].T)
+            ax[i].legend(un_types)
             for ev in ut.trial_events:
-                ax.axvline(events[ev], lw=2, color='k')
-            """
-            ax[1].set_title(str(dpca.explained_variance_ratio_[comp][1]*100))
-            ax[1].plot(time, R_transform[comp][1, :, :].T)
-            for ev in ut.trial_events:
-                ax[1].axvline(events[ev], lw=2, color='k')
-            """
+                ax[i].axvline(events[ev], lw=2, color='k')
+        f.suptitle(color)
         f.tight_layout()
 
-        l = []
-        f, ax = plt.subplots(1, 2)
-        for k in R_transform.keys():
-            ax[0].plot(np.cumsum(np.array(dpca.explained_variance_ratio_[k])*100))
-            l.append(k)
-        ax[0].legend(l)
-        ax[0].set_xlabel("PC")
-        ax[0].set_ylabel("% variance explained")
-
-        l = []
-        for k in R_transform.keys():
-            if k != 't':
-                ax[1].plot(np.cumsum(np.array(dpca.explained_variance_ratio_[k])*100))
-                l.append(k)
-        ax[1].legend(l)
-        ax[1].set_xlabel("PC")
-        ax[1].set_ylabel("% variance explained")
-
-        f.suptitle(comp+", "+color)
+        f, ax = plt.subplots(1, 1)
+        c = ['black', 'gold']
+        for i, k in enumerate(R_transform.keys()):
+            ax.plot(np.cumsum(np.array(dpca.explained_variance_ratio_[k])*100), 'o-', color=c[i])
+        ax.legend(["Condition independent", "Condition dependent"])
+        ax.set_xlabel("PC")
+        ax.set_ylabel("% variance explained")
+        ax.set_ylim((0, 100))
+        ax.axhline(0, linestyle='--', color='k')
+        ax.set_title(color)
         f.tight_layout()
 
 
