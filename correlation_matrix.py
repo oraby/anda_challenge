69657bb179c681074d7da9fbafb7af84e2f9aa31
crheller <charlieheller95@gmail.com>
2019-04-11 11:38:39 +0200

    making correlation matrices

--- correlation_matrix.py
+++ correlation_matrix.py
@@ -6,16 +6,18 @@ import utils as ut
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.ndimage as sf
+import scipy.cluster.hierarchy as sch
 
-def computeCorrelationMatrix(colors=ut.COLORS):
+def computeCorrelationMatrix(colors=ut.COLORS, fs=50):
     # compute correlation matrix for range of different time delays
     f, ax = plt.subplots(1, 1)
+    corr_mat = {}
     for i, c in enumerate(colors):
         print("preprocessing: {}".format(c))
         block = ut.load_dataset(c)
         block = ut.sort_spiketrains(block)
-        r = ut.rasterize_data(block, sf=50)
-        r = ut.calculate_single_trial_PSTH(r, fs=50)
+        r = ut.rasterize_data(block, sf=fs)
+        r = ut.calculate_single_trial_PSTH(r, fs=fs)
         n_units = r.shape[1]
         r = r.transpose(1, 0, 2).reshape(n_units, -1)
         print("Calculating...")
@@ -24,6 +26,7 @@ def computeCorrelationMatrix(colors=ut.COLORS):
         sig = sig_mat < 1 #0.05
         print("% significant: {0} for c: {1}".format(sig.sum() / sig.shape[0], c))
         cc = np.corrcoef(r)
+        corr_mat[c] = cc.copy()
         cc = cc[~np.eye(cc.shape[0],dtype=bool)]
         sig = sig[~np.eye(sig.shape[0],dtype=bool)]
         cc = cc[sig]
@@ -41,10 +44,44 @@ def computeCorrelationMatrix(colors=ut.COLORS):
             cc_bin.append(np.mean(cc[args]))
         #axes[ax[i]].imshow(cc_sorted, aspect='auto', cmap='seismic')
         ax.plot(distance, sf.gaussian_filter1d(cc_bin, 3), 'o-', color=c)
+        ax.set_xlabel("Electrode distance")
+        ax.set_ylabel("Pairwise correlation")
 
     f.tight_layout()
-    plt.show()
+
+
+    return corr_mat
 
 
 if __name__ == "__main__":
-    computeCorrelationMatrix()
+    corr_mat = computeCorrelationMatrix(fs=100)
+
+
+    # plot sorted correlation matrix to look for higher order correlations
+    axes = [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2)]
+    colors = ut.COLORS[::-1]
+    vmin = 0
+    vmax = 0
+    for c in colors:
+        min = np.min(corr_mat[c][~np.eye(corr_mat[c].shape[0],dtype=bool)])
+        max = np.max(corr_mat[c][~np.eye(corr_mat[c].shape[0],dtype=bool)])
+
+        if min < vmin:
+            vmin = min
+        if max > vmax:
+            vmax = max
+
+    f1, ax1 = plt.subplots(2, 3)
+    for i, a in enumerate(axes):
+        cm = corr_mat[colors[i]]
+        Y = sch.linkage((1 - cm).copy(), method='centroid')
+        Z = sch.dendrogram(Y, orientation='right', no_plot=True)
+        index = Z['leaves']
+        cm = cm[:, index][index, :]
+        # fill diagonal with 0s
+        cm[np.diag_indices(cm.shape[0])] = 0
+        im = ax1[a].imshow(cm, aspect='auto', cmap='seismic', vmin=vmin, vmax=vmax)
+        ax1[a].set_title(colors[i])
+    f1.colorbar(im)
+    f1.tight_layout()
+    plt.show()
